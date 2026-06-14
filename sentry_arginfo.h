20162d6bb38ac1bb31b73549020bd1a89b7827bb
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-06-14 14:54:01 +0200

    update arginfo

--- sentry_arginfo.h
+++ sentry_arginfo.h
@@ -1,20 +1,35 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: f7b643bda50e8238549b1d7d38f2a9dd9645e616 */
+ * Stub hash: 0aec23779b930567dc122353b56e89b7bfb87520 */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_Sentry_trace, 0, 3, _IS_BOOL, 0)
-	ZEND_ARG_TYPE_INFO(0, class, IS_STRING, 1)
-	ZEND_ARG_TYPE_INFO(0, function, IS_STRING, 0)
-	ZEND_ARG_OBJ_INFO(0, closure, Closure, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_Sentry_instrument, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, class_name, IS_STRING, 1)
+	ZEND_ARG_TYPE_INFO(0, function_name, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, extra_metadata, IS_ARRAY, 0, "[]")
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_sentry, 0, 0, IS_VOID, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_Sentry_setEndCallback, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, callback, IS_CALLABLE, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_FUNCTION(Sentry_trace);
-ZEND_FUNCTION(sentry);
+#define arginfo_Sentry_setStartCallback arginfo_Sentry_setEndCallback
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Sentry_Trace___construct, 0, 0, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, metadata, IS_ARRAY, 0, "[]")
+ZEND_END_ARG_INFO()
+
+ZEND_FUNCTION(Sentry_instrument);
+ZEND_FUNCTION(Sentry_setEndCallback);
+ZEND_FUNCTION(Sentry_setStartCallback);
+ZEND_METHOD(Sentry_Trace, __construct);
 
 static const zend_function_entry ext_functions[] = {
-	ZEND_RAW_FENTRY(ZEND_NS_NAME("Sentry", "trace"), zif_Sentry_trace, arginfo_Sentry_trace, 0, NULL, NULL)
-	ZEND_FE(sentry, arginfo_sentry)
+	ZEND_RAW_FENTRY(ZEND_NS_NAME("Sentry", "instrument"), zif_Sentry_instrument, arginfo_Sentry_instrument, 0, NULL, NULL)
+	ZEND_RAW_FENTRY(ZEND_NS_NAME("Sentry", "setEndCallback"), zif_Sentry_setEndCallback, arginfo_Sentry_setEndCallback, 0, NULL, NULL)
+	ZEND_RAW_FENTRY(ZEND_NS_NAME("Sentry", "setStartCallback"), zif_Sentry_setStartCallback, arginfo_Sentry_setStartCallback, 0, NULL, NULL)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Sentry_Trace_methods[] = {
+	ZEND_ME(Sentry_Trace, __construct, arginfo_class_Sentry_Trace___construct, ZEND_ACC_PUBLIC)
 	ZEND_FE_END
 };
