bd41748032b2a05e9e3503b8930f22ce74c5437c
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-06-14 16:12:03 +0200

    array shape in stub

--- sentry.stub.php
+++ sentry.stub.php
@@ -3,6 +3,9 @@
 /**
  * @generate-function-entries
  * @generate-legacy-arginfo 80000
+ *
+ * @phpstan-type StartData array{name: string, start_time: float, metadata: array<string, mixed>}
+ * @phpstan-type EndData array{name: string, start_time: float, end_time: float, duration: float, metadata: array<string, mixed>}
  */
 
 namespace Sentry {
@@ -12,8 +15,10 @@ namespace Sentry {
         array $extra_metadata = []
     ): bool {}
 
+    /** @param callable(EndData, mixed): mixed $callback */
     function setEndCallback(callable $callback): bool {}
 
+    /** @param callable(StartData): mixed $callback */
     function setStartCallback(callable $callback): bool {}
 
     #[\Attribute(\Attribute::TARGET_FUNCTION | \Attribute::TARGET_METHOD)]
